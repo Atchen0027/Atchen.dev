4a2aa660897e55e0330e44db0833699014c5a9f4
Atchen Valladares Alonso <acen0027@gmail.com>
2025-12-09 20:23:13 +0000

    formularios

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,22 +4,25 @@ export const Contact = () => {
       <div class="container">
         <h2 class="section-title">¿Tienes dudas? Empecemos a hablar de tu proyecto.</h2>
         <div class="contact-wrapper fade-in">
-          <form class="contact-form" onsubmit="event.preventDefault(); alert('Mensaje enviado (Simulado)');">
+          <form class="contact-form" action="https://formsubmit.co/acen0027@gmail.com" method="POST">
+            <input type="hidden" name="_subject" value="Nuevo contacto desde Atchen Landing">
+            <input type="hidden" name="_captcha" value="false">
+            
             <div class="form-group">
               <label for="name">Nombre</label>
-              <input type="text" id="name" placeholder="Tu nombre" required>
+              <input type="text" id="name" name="name" placeholder="Tu nombre" required>
             </div>
             <div class="form-group">
               <label for="email">Email</label>
-              <input type="email" id="email" placeholder="tucorreo@ejemplo.com" required>
+              <input type="email" id="email" name="email" placeholder="tucorreo@ejemplo.com" required>
             </div>
              <div class="form-group">
               <label for="phone">Teléfono (Opcional)</label>
-              <input type="tel" id="phone" placeholder="+34 600...">
+              <input type="tel" id="phone" name="phone" placeholder="+34 600...">
             </div>
             <div class="form-group">
               <label for="message">Mensaje</label>
-              <textarea id="message" rows="5" placeholder="Cuéntanos sobre tu proyecto..." required></textarea>
+              <textarea id="message" name="message" rows="5" placeholder="Cuéntanos sobre tu proyecto..." required></textarea>
             </div>
             <button type="submit" class="btn btn-primary btn-block">ENVIAR CONSULTA</button>
           </form>
