686bd5de124fb73d1f3009e221529634a6a91fd0
Atchen Valladares Alonso <acen0027@gmail.com>
2025-12-12 19:50:34 +0000

    Add GitHub Actions deployment workflow and tutorials page

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,5 @@
 import { defineConfig } from "vite";
-import path from "path";
 
 export default defineConfig({
-  base: "/atchen-landing-page/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),
-        tutorials: path.resolve(__dirname, "public/tutorials.html"),
-      },
-    },
-  },
+  base: "./",
 });
